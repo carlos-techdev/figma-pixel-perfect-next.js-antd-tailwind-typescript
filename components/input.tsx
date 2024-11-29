30baf8d765e43549ce5eb82a63aeaccccb4c3597
Yeah <yeah@gmail.com>
2024-11-29 10:55:54 -0800

    feat: component/input added

---
+++ components/input.tsx
@@ -0,0 +1,22 @@
+"use client"
+
+import { Input } from "antd"
+import MyIcon from "components/icon"
+
+export default function MyInput() {
+  return (
+    <div>
+      <Input
+        size="large"
+        placeholder="Search"
+        prefix={<MyIcon type="magnifying" />}
+        suffix={
+          <div className="flex items-center space-x-2">
+            <MyIcon type="command" />
+            <MyIcon type="f" />
+          </div>
+        }
+      />
+    </div>
+  )
+}
