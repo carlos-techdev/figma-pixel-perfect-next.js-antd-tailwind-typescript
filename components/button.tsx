a3e023663813b17e475a2ad6c087b776a1157ce4
harry9123 <eth.domperin9123@gmail.com>
2024-12-03 00:39:14 -0800

    Revert "Dev"

--- components/button.tsx
+++
@@ -1,8 +0,0 @@
-import { Button } from "antd"
-
-export const btn_group = (
-  <div className="space-x-2">
-    <Button className="h-10 w-[98px] text-[12px] font-semibold leading-[18px] text-[#0d0d12]">Cancel</Button>
-    <Button className="h-10 w-[104px] bg-black text-[12px] font-bold leading-[18px] text-white">Save Change</Button>
-  </div>
-)
