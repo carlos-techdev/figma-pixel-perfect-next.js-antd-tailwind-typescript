10282c8bb371290f8db0554f2e3c42b5cee3c8f2
carlos <carlos.santoscode@gmail.com>
2024-12-16 10:11:08 -0800

    fix: components/dragger fixed

--- components/dragger.tsx
+++ components/dragger.tsx
@@ -1,28 +1,7 @@
-import { message, Upload } from "antd"
-import type { UploadProps } from "antd"
+import { Upload } from "antd"
 import MyIcon from "./icon"
 
 const { Dragger } = Upload
-
-// const props: UploadProps = {
-//   name: "file",
-//   multiple: true,
-//   action: "https://660d2bd96ddfa2943b33731c.mockapi.io/api/upload",
-//   onChange(info) {
-//     const { status } = info.file
-//     if (status !== "uploading") {
-//       console.log(info.file, info.fileList)
-//     }
-//     if (status === "done") {
-//       message.success(`${info.file.name} file uploaded successfully.`)
-//     } else if (status === "error") {
-//       message.error(`${info.file.name} file upload failed.`)
-//     }
-//   },
-//   onDrop(e) {
-//     console.log("Dropped files", e.dataTransfer.files)
-//   },
-// }
 
 const MyDragger = () => {
   return (
