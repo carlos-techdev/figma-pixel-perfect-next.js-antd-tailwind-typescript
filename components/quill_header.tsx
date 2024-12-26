79b6fc33d38a2b78fe313e38d1bea4360ecd1a22
carlos <carlos.santoscode@gmail.com>
2024-12-26 09:25:05 -0800

    feat: components/quill_header added

---
+++ components/quill_header.tsx
@@ -0,0 +1,81 @@
+export const QuillHeader = () => (
+  <div id="toolbar" className="mb-2 rounded border border-[#e4e4e4] p-2">
+    <select className="ql-header" defaultValue="">
+      <option value="1">Header 1</option>
+      <option value="2">Header 2</option>
+      <option value="">Normal</option>
+    </select>
+    <button className="ql-bold"></button>
+    <button className="ql-italic"></button>
+    <button className="ql-underline"></button>
+    <button className="ql-strike"></button>
+    <button className="ql-blockquote"></button>
+    <button className="ql-code-block"></button>
+    <button className="ql-list" value="ordered"></button>
+    <button className="ql-list" value="bullet"></button>
+    <button className="ql-align" value=""></button>
+    <button className="ql-align" value="center"></button>
+    <button className="ql-align" value="right"></button>
+    <button className="ql-align" value="justify"></button>
+    <button className="ql-indent" value="-1"></button>
+    <button className="ql-indent" value="+1"></button>
+    <button className="ql-script" value="sub"></button>
+    <button className="ql-script" value="super"></button>
+    <select className="ql-color">
+      <option value="red"></option>
+      <option value="green"></option>
+      <option value="blue"></option>
+      <option value="custom-color"></option>
+    </select>
+    <select className="ql-background">
+      <option value="yellow"></option>
+      <option value="lightgreen"></option>
+      <option value="lightblue"></option>
+    </select>
+    <button className="ql-link"></button>
+    <button className="ql-image"></button>
+    <button className="ql-video"></button>
+    <button className="ql-clean"></button>
+  </div>
+)
+
+export const QuillHeader_1 = () => (
+  <div id="toolbar_1" className="mb-2 rounded border border-[#e4e4e4] p-2">
+    <select className="ql-header" defaultValue="">
+      <option value="1">Header 1</option>
+      <option value="2">Header 2</option>
+      <option value="">Normal</option>
+    </select>
+    <button className="ql-bold"></button>
+    <button className="ql-italic"></button>
+    <button className="ql-underline"></button>
+    <button className="ql-strike"></button>
+    <button className="ql-blockquote"></button>
+    <button className="ql-code-block"></button>
+    <button className="ql-list" value="ordered"></button>
+    <button className="ql-list" value="bullet"></button>
+    <button className="ql-align" value=""></button>
+    <button className="ql-align" value="center"></button>
+    <button className="ql-align" value="right"></button>
+    <button className="ql-align" value="justify"></button>
+    <button className="ql-indent" value="-1"></button>
+    <button className="ql-indent" value="+1"></button>
+    <button className="ql-script" value="sub"></button>
+    <button className="ql-script" value="super"></button>
+    <select className="ql-color">
+      <option value="red"></option>
+      <option value="green"></option>
+      <option value="blue"></option>
+      <option value="custom-color"></option>
+    </select>
+    <select className="ql-background">
+      <option value="yellow"></option>
+      <option value="lightgreen"></option>
+      <option value="lightblue"></option>
+    </select>
+    <button className="ql-link"></button>
+    <button className="ql-image"></button>
+    <button className="ql-video"></button>
+    <button className="ql-clean"></button>
+  </div>
+)
